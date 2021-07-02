10a46dd7ed84864ea2e0cf5113823a1da51c5068
johnsoe <evan.johnson.r@gmail.com>
2021-07-01 19:42:59 -0700

    Add main league view

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import React from 'react'
+import MarbleLeague from './components/MarbleLeague'
+import axios from 'axios'
+
+
+class App extends React.Component {
+
+  constructor() {
+    super();
+    this.state = { teams: null };
+  }
+
+  componentDidMount() {
+    axios.get("https://lsgs3tdrt3.execute-api.us-west-2.amazonaws.com/dev", {
+      headers: { 'Content-Type': 'application/json' }
+    })
+      .then(res => {
+        //console.log(res.data)
+        this.setState({
+          teams : res.data.body
+        })
+      })
+      .catch(err => console.log(err));
+  }
+
+  render() {
+    return (
+      <div className="Top-ML-Container">
+          { this.state.teams &&
+            <MarbleLeague teams={
+              this.state.teams.filter(item => item.ml_team)
+            } />
+          }
+      </div>
+    );
+  }
 }
+// <header className="App-header">
+//   <img src={logo} className="App-logo" alt="logo" />
+//   <p>
+//     Edit <code>src/App.js</code> and save to reload.
+//   </p>
+//   <a
+//     className="App-link"
+//     href="https://reactjs.org"
+//     target="_blank"
+//     rel="noopener noreferrer"
+//   >
+//     Learn React
+//   </a>
+// </header>
 
 export default App;
