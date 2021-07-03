6311e6a4dc2d552b8957c55a43ea39c1620d9c69
johnsoe <evan.johnson.r@gmail.com>
2021-07-02 23:03:03 -0700

    More auth changes, again

--- src/App.js
+++ src/App.js
@@ -5,10 +5,39 @@ import MarbleLeague from './components/MarbleLeague'
 import axios from 'axios'
 
 import Amplify, { Auth, Hub } from 'aws-amplify';
-import awsconfig from './aws-exports';
+import awsConfig from './aws-exports';
 import { AmplifySignOut, withAuthenticator } from '@aws-amplify/ui-react';
 
-Amplify.configure(awsconfig);
+const isLocalhost = Boolean(
+  window.location.hostname === "localhost" ||
+    // [::1] is the IPv6 localhost address.
+    window.location.hostname === "[::1]" ||
+    // 127.0.0.1/8 is considered localhost for IPv4.
+    window.location.hostname.match(
+      /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
+    )
+);
+
+// Assuming you have two redirect URIs, and the first is for localhost and second is for production
+const [
+  localRedirectSignIn,
+  productionRedirectSignIn,
+] = awsConfig.oauth.redirectSignIn.split(",");
+
+const [
+  localRedirectSignOut,
+  productionRedirectSignOut,
+] = awsConfig.oauth.redirectSignOut.split(",");
+
+const updatedAwsConfig = {
+  ...awsConfig,
+  oauth: {
+    ...awsConfig.oauth,
+    redirectSignIn: isLocalhost ? localRedirectSignIn : productionRedirectSignIn,
+    redirectSignOut: isLocalhost ? localRedirectSignOut : productionRedirectSignOut,
+  }
+}
+Amplify.configure(updatedAwsConfig);
 
 class App extends React.Component {
 
